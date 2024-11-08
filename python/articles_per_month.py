60d40085bc842ff9665e8ef5df8b9b67336a3271
Louna Camas <lounacamas@gmail.com>
2024-11-08 14:39:51 +0100

    Articles per month

--- python/articles_per_month.py
+++ python/articles_per_month.py
@@ -1,1 +1,51 @@
+'''
+Barchart of number of articles per month
 
+'''
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import calendar
+from datetime import datetime
+
+file_name = "topaz-data732--france--www.fdesouche.com--20190101--20211231.json"
+
+f = open("data/"+file_name, 'r', encoding='utf-8')
+df = json.loads(f.read())
+f.close()
+
+articles_per_month = []
+
+data_months = df["data-all"]
+for year, months in data_months.items():
+    for month, cat in months.items():
+        name_month = calendar.month_name[int(month)]
+        articles_count = sum(len(cat[c]) for c in cat)
+        articles_per_month.append([f"{year}-{name_month}",articles_count])
+ 
+sorted_data = sorted(articles_per_month, key=lambda x: datetime.strptime((x[0]), "%Y-%B"))        
+        
+months = [sorted_data[i][0] for i in range(len(sorted_data))]
+counts = [sorted_data[j][1] for j in range (len(sorted_data))]
+
+fig = go.Figure(data=[
+    go.Bar(x=months,
+           y=counts,
+           marker=dict(
+                color=counts, 
+                colorscale="Greens",
+                colorbar=dict(title="Number of Articles")  # Afficher une barre de couleur à droite
+            ),
+            hovertemplate="<b>Date:</b> %{x}<br>" + "<b>Number of articles:</b> %{y}<extra></extra>"
+    )
+])
+
+fig.update_layout(
+    title="Articles per month",
+    xaxis_title="Month",
+    yaxis_title="Number of articles",
+    xaxis_tickangle=-45,
+    template="plotly_white"
+)
+
+fig.show()
--- python/frequent_keywords.py
+++ python/frequent_keywords.py
@@ -1,1 +1,6 @@
+from datetime import datetime
 
+# Test simple avec un format 'YYYY-Month'
+date_str = '2021-January'
+date_obj = datetime.strptime(date_str, "%Y-%B")
+print(date_obj)
